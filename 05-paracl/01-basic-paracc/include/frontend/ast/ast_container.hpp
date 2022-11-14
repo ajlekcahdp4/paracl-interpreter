36c79a00cf7a6549da489766f8b450e3732f51a2
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-11-14 21:23:30 +0300

    move ast_container make_ function defenitions into separate file and change them

--- 05-paracl/01-basic-paracc/include/frontend/ast/ast_container.hpp
+++ 05-paracl/01-basic-paracc/include/frontend/ast/ast_container.hpp
@@ -18,44 +18,28 @@ namespace paracl::frontend::ast {
 struct ast_container {
   std::vector<i_ast_node_uptr> m_nodes;
 
-  static i_ast_node *make_assignment_statement(variable_expression *left, i_ast_node *right, location l) {
-    return new assignment_statement{left, right, l};
-  }
+  assignment_statement *make_assignment_statement(variable_expression *left, i_ast_node *right, location l);
 
-  static i_ast_node *make_binary_expression(binary_operation op_type, i_ast_node *left, i_ast_node *right, location l) {
-    return new binary_expression{op_type, left, right, l};
-  }
+  binary_expression *make_binary_expression(binary_operation op_type, i_ast_node *left, i_ast_node *right, location l);
 
-  static i_ast_node *make_constant_expression(int val, location l) { return new constant_expression{val, l}; }
+  constant_expression *make_constant_expression(int val, location l);
 
-  static i_ast_node *make_error_node(std::string msg, location l) { return new error_node{msg, l}; }
+  error_node *make_error_node(std::string msg, location l);
 
-  static i_ast_node *make_if_statement(i_ast_node *cond, i_ast_node *true_block, location l) {
-    return new if_statement{cond, true_block, l};
-  }
+  if_statement *make_if_statement(i_ast_node *cond, i_ast_node *true_block, location l);
 
-  static i_ast_node *make_if_statement(i_ast_node *cond, i_ast_node *true_block, i_ast_node *else_block, location l) {
-    return new if_statement{cond, true_block, else_block, l};
-  }
+  if_statement *make_if_statement(i_ast_node *cond, i_ast_node *true_block, i_ast_node *else_block, location l);
 
-  static i_ast_node *make_print_statement(i_ast_node *expr, location l) { return new print_statement{expr, l}; }
+  print_statement *make_print_statement(i_ast_node *expr, location l);
 
-  static i_ast_node *make_read_expression(location l) { return new read_expression{l}; }
+  read_expression *make_read_expression(location l);
 
-  static i_ast_node *make_statement_block(std::vector<i_ast_node *> &&vec, location l) {
-    return new statement_block{std::move(vec), l};
-  }
+  statement_block *make_statement_block(std::vector<i_ast_node *> &&vec, location l);
 
-  static i_ast_node *make_unary_expression(unary_operation op, i_ast_node *expr, location l) {
-    return new unary_expression{op, expr, l};
-  }
+  unary_expression *make_unary_expression(unary_operation op, i_ast_node *expr, location l);
 
-  static i_ast_node *make_variable_expression(std::string &name, location l) {
-    return new variable_expression{name, l};
-  }
+  variable_expression *make_variable_expression(std::string &name, location l);
 
-  static i_ast_node *make_while_statement(i_ast_node *cond, i_ast_node *block, location l) {
-    return new while_statement{cond, block, l};
-  }
+  while_statement *make_while_statement(i_ast_node *cond, i_ast_node *block, location l);
 };
 } // namespace paracl::frontend::ast
---
+++ 05-paracl/01-basic-paracc/src/frontend/ast_container.cc
@@ -0,0 +1,80 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <tsimmerman.ss@phystech.edu>, <alex.rom23@mail.ru> wrote this file.  As long as you
+ * retain this notice you can do whatever you want with this stuff. If we meet
+ * some day, and you think this stuff is worth it, you can buy me a beer in
+ * return.
+ * ----------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include "ast_container.hpp"
+
+namespace paracl::frontend::ast {
+
+assignment_statement *ast_container::make_assignment_statement(variable_expression *left, i_ast_node *right,
+                                                               location l) {
+  m_nodes.emplace_back(new assignment_statement{left, right, l});
+  return static_cast<assignment_statement *>(m_nodes.back().get());
+}
+
+binary_expression *ast_container::make_binary_expression(binary_operation op_type, i_ast_node *left, i_ast_node *right,
+                                                         location l) {
+  m_nodes.emplace_back(new binary_expression{op_type, left, right, l});
+  return static_cast<binary_expression *>(m_nodes.back().get());
+}
+
+constant_expression *ast_container::make_constant_expression(int val, location l) {
+  m_nodes.emplace_back(new constant_expression{val, l});
+  return static_cast<constant_expression *>(m_nodes.back().get());
+}
+
+error_node *ast_container::make_error_node(std::string msg, location l) {
+  m_nodes.emplace_back(new error_node{msg, l});
+  return static_cast<error_node *>(m_nodes.back().get());
+}
+
+if_statement *ast_container::make_if_statement(i_ast_node *cond, i_ast_node *true_block, location l) {
+  m_nodes.emplace_back(new if_statement{cond, true_block, l});
+  return static_cast<if_statement *>(m_nodes.back().get());
+}
+
+if_statement *ast_container::make_if_statement(i_ast_node *cond, i_ast_node *true_block, i_ast_node *else_block,
+                                               location l) {
+  m_nodes.emplace_back(new if_statement{cond, true_block, l});
+  return static_cast<if_statement *>(m_nodes.back().get());
+}
+
+print_statement *ast_container::make_print_statement(i_ast_node *expr, location l) {
+  m_nodes.emplace_back(new print_statement{expr, l});
+  return static_cast<print_statement *>(m_nodes.back().get());
+}
+
+read_expression *ast_container::make_read_expression(location l) {
+  m_nodes.emplace_back(new read_expression{l});
+  return static_cast<read_expression *>(m_nodes.back().get());
+}
+
+statement_block *ast_container::make_statement_block(std::vector<i_ast_node *> &&vec, location l) {
+  m_nodes.emplace_back(new statement_block{std::move(vec), l});
+  return static_cast<statement_block *>(m_nodes.back().get());
+}
+
+unary_expression *ast_container::make_unary_expression(unary_operation op, i_ast_node *expr, location l) {
+  m_nodes.emplace_back(new unary_expression{op, expr, l});
+  return static_cast<unary_expression *>(m_nodes.back().get());
+}
+
+variable_expression *ast_container::make_variable_expression(std::string &name, location l) {
+  m_nodes.emplace_back(new variable_expression{name, l});
+  return static_cast<variable_expression *>(m_nodes.back().get());
+}
+
+while_statement *ast_container::make_while_statement(i_ast_node *cond, i_ast_node *block, location l) {
+  m_nodes.emplace_back(new while_statement{cond, block, l});
+  return static_cast<while_statement *>(m_nodes.back().get());
+}
+
+} // namespace paracl::frontend::ast
